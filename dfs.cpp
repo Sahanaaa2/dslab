3deeadc29155f03976a6eba2060091ab3b3d702b
Sahanaaa2 <149652287+Sahanaaa2@users.noreply.github.com>
2024-01-03 11:20:38 +0530

    dfs.cpp

--- dfs.cpp
+++ dfs.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main()
-{                                                                                                  //variable declaration
+{                                                                                //variable declaration
     int arr[10][10], i, j, k, n, e, top, v, stk[10], visit[10], visited[10];
 
     cout << "Enter the number of vertices in the Graph: ";
@@ -23,12 +23,12 @@ int main()
     cout << "\nThe DFS traversal on the given graph is : \n";
     cout << v << " ";
 
-    																							//As we start with the vertex v, marking it visited to avoid visiting again
+    																		   //As we start with the vertex v, marking it visited to avoid visiting again
     visited[v] = 1;
 
     k = 1;
 
-    																			//The DFS Traversal Logic
+    																		  //The DFS Traversal Logic
     while (k < n)
     {
         for (j = n; j >= 1; j--)
@@ -37,13 +37,13 @@ int main()
             {
                 visit[j] = 1;
 
-                															//put all the vertices that are connected to the visited vertex into a stack
+                															 //put all the vertices that are connected to the visited vertex into a stack
                 stk[top] = j;
                 top++;
             }
         }
 
-        																	//output all the connected vertices one at a time
+        																	 //output all the connected vertices one at a time
         v = stk[--top];
         cout << v << " ";
         k++;
@@ -51,7 +51,7 @@ int main()
         																	//as v is visited so it is not a valid candidate to visit in future so visit[v]=0 and visited[v]=1
         visit[v] = 0;
 
-       																 //to mark it visited 
+       																       //to mark it visited 
         visited[v] = 1;
     }
 
